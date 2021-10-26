bc1ecf1db88e609e645c24636bd03d8efc73b4f5
Vagyasri <vagyasri2011@gmail.com>
2021-10-26 21:30:45 +0200

    Make differences between hands

--- src/index.js
+++ src/index.js
@@ -9,11 +9,14 @@ const setTime = () => {
     const seconds = now.getSeconds();
     const secDegrees = ((seconds/60) * 360) + 90;
     secondHand.style.transform = `rotate(${secDegrees}deg)`;
+    secondHand.style.height = `3px`;
+
     const mins = now.getMinutes();
     const minDegrees = ((mins/60) * 360) + 90;
     minHand.style.transform = `rotate(${minDegrees}deg)`;
     const hourDegrees = ((mins/12) * 360) + 90;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    hourHand.style.height = `10px`;
 }
 
 setInterval(setTime,1000);
