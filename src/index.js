9bebaaa2cebda6c0fbbfc74a2590a68a1bda0799
Vagyasri <vagyasri2011@gmail.com>
2021-10-26 22:09:35 +0200

    Fix linter error

--- src/index.js
+++ src/index.js
@@ -5,20 +5,20 @@ const minHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
 const setTime = () => {
-    const now = new Date();
-    const seconds = now.getSeconds();
-    const secDegrees = ((seconds/60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secDegrees}deg)`;
-    secondHand.style.height = `3px`;
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secDegrees = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secDegrees}deg)`;
+  secondHand.style.height = '3px';
 
-    const mins = now.getMinutes();
-    const minDegrees = ((mins/60) * 360) + 90;
-    minHand.style.transform = `rotate(${minDegrees}deg)`;
-    
-    const hours = now.getHours();
-    const hourDegrees = ((hours/12) * 360) + 90;
-    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-    hourHand.style.height = `10px`;
-}
+  const mins = now.getMinutes();
+  const minDegrees = (mins / 60) * 360 + 90;
+  minHand.style.transform = `rotate(${minDegrees}deg)`;
 
-setInterval(setTime,1000);
+  const hours = now.getHours();
+  const hourDegrees = (hours / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  hourHand.style.height = '10px';
+};
+
+setInterval(setTime, 1000);
