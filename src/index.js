c83fae5e3820a5cd5667c5acd30fe3793c298a14
Vagyasri <vagyasri2011@gmail.com>
2021-10-26 21:22:14 +0200

    Reflect Real time

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import './style.css';
 
-const setDate = () => {
-    console.log('Hi');
+const secondHand = document.querySelector('.sec-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+const setTime = () => {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secDegrees = ((seconds/60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secDegrees}deg)`;
+    const mins = now.getMinutes();
+    const minDegrees = ((mins/60) * 360) + 90;
+    minHand.style.transform = `rotate(${minDegrees}deg)`;
+    const hourDegrees = ((mins/12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
-setInterval(setDate,1000);
+setInterval(setTime,1000);
