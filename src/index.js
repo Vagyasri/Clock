009df44de8b856274bef395b7aff3032c4f02346
Vagyasri <vagyasri2011@gmail.com>
2021-10-26 21:58:45 +0200

    Adjust hour

--- src/index.js
+++ src/index.js
@@ -14,7 +14,9 @@ const setTime = () => {
     const mins = now.getMinutes();
     const minDegrees = ((mins/60) * 360) + 90;
     minHand.style.transform = `rotate(${minDegrees}deg)`;
-    const hourDegrees = ((mins/12) * 360) + 90;
+    
+    const hours = now.getHours();
+    const hourDegrees = ((hours/12) * 360) + 90;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
     hourHand.style.height = `10px`;
 }
